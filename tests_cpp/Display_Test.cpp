fda70e62db79e19c833feb1b2ef28b8fbdef1e04
Russ Taylor <russ@reliasolve.com>
2024-05-17 16:59:12 -0400

    Adding test file

---
+++ tests_cpp/Display_Test.cpp
@@ -0,0 +1,65 @@
+/*
+ * Copyright (C) 2024: Arizona Board of Regents on Behalf of the University of Arizona
+ */
+
+#include <iostream>
+#include <vector>
+#include <memory>
+#include <Composite.h>
+#include <Display.h>
+#include <ASDP_Core_API.h>
+
+int main()
+{
+  int width = 640;
+  int height = 640;
+  double degreesPerSecond = 15.0;
+
+  asdp::render::ViewRenderInfo viewRenderInfo;
+  viewRenderInfo.width = width;
+  viewRenderInfo.height = height;
+  std::vector<asdp::render::ViewRenderInfo> views;
+  views.push_back(viewRenderInfo);
+
+  // Do the remainder inside of a block so that the objects will be destroyed before
+  // exiting.
+  {
+    // Create a client to use the ASDP API that listens on the loopback interface
+    std::shared_ptr<asdp::CoreClient> client = std::make_shared<asdp::CoreClient>("localhost");
+
+    // Create a CompositeCube object to render once the window is open and the context is active.
+    std::shared_ptr<asdp::render::CompositeCube> composite = std::make_shared<asdp::render::CompositeCube>(10);
+
+    // Create a Display window to show the CompositeCube object.
+    asdp::render::DisplayWindow window("Display_Test", composite, client, 0, 0, width, height);
+    if (window.GetStatus() != "") {
+      std::cerr << "Error opening first display: " << window.GetStatus() << std::endl;
+      return 1;
+    }
+
+    // Create a second Display window to show the same CompositeCube object that shares objects
+    // with the original window's context.
+    asdp::render::DisplayWindow window2("Display_Test2", composite, client, 0, 0, width, height,
+      90, "", &window);
+    if (window2.GetStatus() != "") {
+      std::cerr << "Error opening second display: " << window2.GetStatus() << std::endl;
+      return 2;
+    }
+
+    // Done with the composite object -- let the display objects take over destroying it+.
+    composite.reset();
+
+    // Loop until the user closes both windows.
+    std::cout << "You should see a square of varying-brightness green squares in two windows." << std::endl;
+    std::cout << "You should be able to move and resize the windows, with the display updating." << std::endl;
+    std::cout << "You should be able to rotate the views by pressing the arrow keys." << std::endl;
+    std::cout << "Close the windows to exit." << std::endl;
+    auto start = std::chrono::steady_clock::now();
+    while ((window.GetStatus() == "") || (window2.GetStatus() == "")) {
+      std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    }
+  }
+
+  // Done
+  return 0;
+}
