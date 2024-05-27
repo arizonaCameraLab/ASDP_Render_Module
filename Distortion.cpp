5bbe4808f833e1bfc4588caab07d2d29df1c6b4a
Russ Taylor <russ@reliasolve.com>
2024-05-27 14:57:11 -0400

    Adding Distortion base class to provide an interface for distortion correction along with two derived classes: identity distortion and bilinear interpolation radial distortion away from a center of projection

---
+++ Distortion.cpp
@@ -0,0 +1,162 @@
+/*
+ * Copyright (C) 2024: Arizona Board of Regents on Behalf of the University of Arizona
+ */
+
+#include <cmath>
+#include <iostream>
+#include <Distortion.h>
+using namespace asdp::render;
+
+DistortionRadialLERP::DistortionRadialLERP(std::array<double, 2> const& COP, std::vector<std::array<double, 2>> const& controlPoints)
+  : m_COP(COP), m_ControlPoints(controlPoints)
+{
+  if (m_ControlPoints.size() < 2) {
+    std::cerr << "DistortionRadialLERP: Not enough control points: " << std::to_string(m_ControlPoints.size()) << std::endl;
+  }
+  if (m_ControlPoints.front()[0] != 0) {
+    std::cerr << "DistortionRadialLERP: First control point distance must be zero: " << std::to_string(m_ControlPoints.front()[0]) << std::endl;
+  }
+}
+
+std::array<double, 3> DistortionRadialLERP::MapPoint(std::array<double, 3> point) const
+{
+  // If we don't have enough control points, return the point unchanged
+  if (m_ControlPoints.size() < 2) {
+    return point;
+  }
+
+  // If the point is on the Z = 0 plane, return the point unchanged
+  if (point[2] == 0) {
+    return point;
+  }
+
+  // Calculate the vector from the center of projection to the point
+  // Project the vector onto the Z = -1 plane (scales it to Z = -1)
+  double scale = -1 / point[2];
+  std::array<double, 2> vec = {point[0] - m_COP[0], point[1] - m_COP[1]};
+  vec[0] *= scale;
+  vec[1] *= scale;
+  double dist = std::sqrt(vec[0] * vec[0] + vec[1] * vec[1]);
+
+  // If the distance is before the first control point or after the last control point, return the point unchanged
+  if (dist < m_ControlPoints.front()[0] || dist > m_ControlPoints.back()[0]) {
+    return point;
+  }
+
+  // Find the control points that bound the distance
+  size_t i = 0;
+  while (i < (m_ControlPoints.size() - 1) && m_ControlPoints[i + 1][0] < dist) {
+    i++;
+  }
+
+  // Interpolate between the control points
+  double t = (dist - m_ControlPoints[i][0]) / (m_ControlPoints[i + 1][0] - m_ControlPoints[i][0]);
+  double d = m_ControlPoints[i][1] * (1 - t) + m_ControlPoints[i + 1][1] * t;
+
+  // Scale the vector by both the inverse of the original scaling and the ratio of the interpolated distance to the
+  // original and add it to the center of projection.  Handle the case where the point is right at the center of projection.
+  double reScale = (1 / scale);
+  if (dist > 0) {
+    reScale *= (d / dist);
+  }
+
+  return {m_COP[0] + vec[0] * reScale, m_COP[1] + vec[1] * reScale, point[2]};
+}
+
+
+//==============================================================================
+// Test and its helper functions
+
+bool isNear(std::array<double, 3> const& a, std::array<double, 3> const& b, double epsilon = 1e-6)
+{
+  return std::abs(a[0] - b[0]) < epsilon && std::abs(a[1] - b[1]) < epsilon && std::abs(a[2] - b[2]) < epsilon;
+}
+
+std::string Distortion::Test()
+{
+  // Test the DistortionNone class
+  {
+    DistortionNone none;
+    std::array<double, 3> point = {1, 2, 3};
+    if (!isNear(none.MapPoint(point), point)) {
+      return "DistortionNone: Point not returned unchanged";
+    }
+  }
+
+  // Test the DistortionRadialLERP class
+  {
+    // Test an identity distortion that does not change anything.
+    std::array<double, 2> COP = {0.5, 0.5};
+    std::vector<std::array<double, 2>> controlPoints = {{0, 0}, {1, 1}};
+    DistortionRadialLERP radialLERP(COP, controlPoints);
+    std::array<double, 3> point = {0.5, 0.5, -1};
+    if (!isNear(radialLERP.MapPoint(point), point)) {
+      return "DistortionRadialLERP: Unit radial point not returned unchanged";
+    }
+    point = {0.5, 0.5, -2};
+    if (!isNear(radialLERP.MapPoint(point), point)) {
+      return "DistortionRadialLERP: Radial point not returned unchanged at Z = 2";
+    }
+    for (double x = -2; x <= 2; x += 0.1) {
+      for (double y = -2; y <= 2; y += 0.1) {
+        point = {x, y, -2};
+        if (!isNear(radialLERP.MapPoint(point), point)) {
+          return "DistortionRadialLERP: Point not returned unchanged at Z = 2";
+        }
+      }
+    }
+  }
+  {
+    // Test a factor-of-2 distortion both inside and outside the unit circle.
+    std::array<double, 2> COP = { 0.5, 0.5 };
+    std::vector<std::array<double, 2>> controlPoints = { {0, 0}, {10, 20} };
+    DistortionRadialLERP radialLERP(COP, controlPoints);
+    std::array<double, 3> point = { 0.5, 0.5, -1 };
+    if (!isNear(radialLERP.MapPoint(point), point)) {
+      return "DistortionRadialLERP: 2x Unit radial point not returned unchanged";
+    }
+    point = { 0.5, 0.5, -2 };
+    if (!isNear(radialLERP.MapPoint(point), point)) {
+      return "DistortionRadialLERP: 2x Radial point not returned unchanged at Z = 2";
+    }
+    for (double x = -2; x <= 2; x += 0.1) {
+      for (double y = -2; y <= 2; y += 0.1) {
+        point = { x, y, -2 };
+        std::array<double, 3> expected = { (x-0.5) * 2 + 0.5, (y-0.5) * 2 + 0.5, -2 };
+        if (!isNear(radialLERP.MapPoint(point), expected)) {
+          return "DistortionRadialLERP: 2x Point not changed as expected at Z = 2";
+        }
+      }
+    }
+    point = { -100, -100, -2 };
+    if (!isNear(radialLERP.MapPoint(point), point)) {
+      return "DistortionRadialLERP: 2x Far point not returned unchanged at Z = 2";
+    }
+  }
+  {
+    // Test a factor-of-3 distortion with a large number of interpolated points.
+    std::array<double, 2> COP = { 0.5, 0.5 };
+    std::vector<std::array<double, 2>> controlPoints;
+    for (double i = 0; i < 10; i += 0.01) {
+      controlPoints.push_back({ i, i * 3 });
+    }
+    DistortionRadialLERP radialLERP(COP, controlPoints);
+    std::array<double, 3> point;
+    for (double x = -2; x <= 2; x += 0.1) {
+      for (double y = -2; y <= 2; y += 0.1) {
+        point = { x, y, -2 };
+        std::array<double, 3> expected = { (x - 0.5) * 3 + 0.5, (y - 0.5) * 3 + 0.5, -2 };
+        if (!isNear(radialLERP.MapPoint(point), expected)) {
+          return "DistortionRadialLERP: 2x large count Point not changed as expected at Z = 2";
+        }
+      }
+    }
+    point = { -100, -100, -2 };
+    if (!isNear(radialLERP.MapPoint(point), point)) {
+      return "DistortionRadialLERP: 2x large count Far point not returned unchanged at Z = 2";
+    }
+  }
+
+  return "";
+}
+
---
+++ Distortion.h
@@ -0,0 +1,81 @@
+/*
+ * Copyright (C) 2024: Arizona Board of Regents on Behalf of the University of Arizona
+ */
+
+ /**
+  * @file Distortion.h
+  * @brief Apache Strap-Down Pilotage Render/Distortion class header file.
+  *
+  * @author ReliaSolve.
+  * @date April 29, 2024.
+  */
+
+#pragma once
+
+#include <array>
+#include <vector>
+#include <string>
+
+namespace asdp {
+  namespace render {
+
+    /// @brief Base class for distortion correction that defines the interface.
+    class Distortion {
+    public:
+
+      /// @brief Virtual destructor so that proper deconstruction happens on pointers.
+      virtual ~Distortion() = default;
+
+      /// @brief Map a point from the ideal camera to the distorted real camera location.
+      /// @param point Point in ideal-camera space.  The distortion map will have been generated
+      /// for an ideal camera of known horizontal and vertical fields of view whose center of
+      /// projection is the center of the image sensor.  The camera will be right-handed with its
+      /// +X axis to the right in the image, its +Y axis pointing up in the image, looking down
+      /// the -Z axis.  The center of projection for the camera is at the origin.  For example.
+      /// the point (0, 0, -5) will project to the center of the image, 5 units away from the camera
+      /// in any ideal camera's space.
+      /// @return Projection point for this pixel in the real camera.  The point will have the same
+      /// Z value as the input point, but the X and Y values will be distorted to account for the
+      /// lens distortion.
+      /// Depending on the distortion model, the point may be shifted, scaled, or rotated.  It may
+      /// lie outside of the field of view of the ideal camera.
+      virtual std::array<double, 3> MapPoint(std::array<double, 3> point) const = 0;
+
+      /// @brief Test function for all subclasses that returns an empty string on success or an error message on failure.
+      static std::string Test();
+    };
+
+    /// @brief Null distortion model that returns the input point unchanged.
+    class DistortionNone : public Distortion {
+    public:
+
+      std::array<double, 3> MapPoint(std::array<double, 3> point) const override { return point; }
+    };
+
+    /// @brief Piecewise linear distortion model that is radial from a center of projection.
+    /// @details This distortion model is a piecewise linear model that is radial from a center of
+    /// projection.  Linear interpolation is performed between control points to determine the
+    /// distortion at any point.
+    class DistortionRadialLERP : public Distortion {
+    public:
+      /// @brief Constructor that takes the center of projection and control points for the distortion.
+      /// @param COP Center of projection for the distortion.  This is the normalized point in the region
+      /// covered by the sensor.  For an ideal camera, the center of the sensor would be (0.5, 0.5).
+      /// @param controlPoints Control points for the distortion.  These points are the radial distance
+      /// from the center of projection and they lie on a plane that is 1 unit down the -Z axis.  The first
+      /// element is the radial distance in the undistored case, and the second element is the radial distance
+      /// in the distorted case.  The control points must start at 0 and be in increasing order of radial distance.
+      /// There must be at least two control points.  Points outside of the range of the control points will be
+      /// left unchanged (remember that the control points must reach all the way to the corners of the image,
+      /// not just the edges).
+      DistortionRadialLERP(std::array<double, 2> const &COP, std::vector<std::array<double, 2>> const &controlPoints);
+
+      std::array<double, 3> MapPoint(std::array<double, 3> point) const override;
+
+    protected:
+      std::array<double, 2> m_COP;
+      std::vector<std::array<double, 2>> m_ControlPoints;
+    };
+
+  } // namespace render
+} // namespace asdp
