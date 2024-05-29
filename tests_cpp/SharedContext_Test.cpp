9754963d3a4f550d0a761bbc8ba47d2b3bca7c74
Russ Taylor <russ@reliasolve.com>
2024-05-29 16:21:30 -0400

    Also using C++ raw-string definition for shaders in test code

--- tests_cpp/SharedContext_Test.cpp
+++ tests_cpp/SharedContext_Test.cpp
@@ -64,25 +64,27 @@ GLfloat vertices[] = {
 GLushort indices[] = { 0, 1, 2, 0, 2, 3 };
 
 // Vertex Shader
-const char* vertexShaderSource = "#version 330 core\n"
-"layout (location = 0) in vec3 aPos;\n"
-"layout (location = 1) in vec2 aTexCoord;\n"
-"out vec2 TexCoord;\n"
-"void main()\n"
-"{\n"
-"   gl_Position = vec4(aPos, 1.0);\n"
-"   TexCoord = vec2(aTexCoord.x, aTexCoord.y);\n"
-"}\n\0";
+const char* vertexShaderSource =
+R"(#version 330 core
+   layout (location = 0) in vec3 aPos;
+   layout (location = 1) in vec2 aTexCoord;
+   out vec2 TexCoord;
+   void main()
+   {
+      gl_Position = vec4(aPos, 1.0);
+      TexCoord = vec2(aTexCoord.x, aTexCoord.y);
+   })";
 
 // Fragment Shader
-const char* fragmentShaderSource = "#version 330 core\n"
-"out vec4 FragColor;\n"
-"in vec2 TexCoord;\n"
-"uniform sampler2D texture1;\n"
-"void main()\n"
-"{\n"
-"   FragColor = texture(texture1, TexCoord);\n"
-"}\n\0";
+const char* fragmentShaderSource =
+R"(#version 330 core
+   out vec4 FragColor;
+   in vec2 TexCoord;
+   uniform sampler2D texture1;
+   void main()
+   {
+      FragColor = texture(texture1, TexCoord);
+   })";
 
 int main()
 {
