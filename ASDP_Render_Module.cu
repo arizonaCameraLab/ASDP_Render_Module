c4a4cdacc656c2b78f447e45d721e8738033d97b
Russ Taylor <russ@reliasolve.com>
2024-08-30 14:22:00 -0400

    Using 3 texture threads when we have more than 21 cameras so that we can keep up.

--- ASDP_Render_Module.cu
+++ ASDP_Render_Module.cu
@@ -1139,8 +1139,9 @@ int main(int argc, char** argv)
 
     // Make additional OpenGL contexts for all but the first texture thread -- re-use the original for
     // the first one.  Testing shows that we can handle up to 13 cameras on a single texture thread,
-    // so we use 2 threads to handle all 25.
-    static const int NUM_TEXTURE_THREADS = 2;
+    // testing shows that 2 threads can handle 21 cameras but we need 3 threads to handle all 25.
+    int NUM_TEXTURE_THREADS = 2;
+    if (cameras.size() > 21) { NUM_TEXTURE_THREADS = 3; }
     std::vector< std::shared_ptr<DisplayTexture> > displayTextures = { displayTexture };
     for (size_t i = 1; i < NUM_TEXTURE_THREADS; i++) {
       std::shared_ptr<DisplayTexture> dt = std::make_shared<DisplayTexture>(displayTexture.get());
