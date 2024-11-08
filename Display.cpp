395ae9e825a6bc3f5d2e1a3e53ae386b1c21fcbb
Russ Taylor <russ@reliasolve.com>
2024-11-08 13:47:19 -0500

    Fixed config file for wFOV in camera 6025. Adjusted far clip to be beyond the wFOV planes, and made near a bit further out.

--- Display.cpp
+++ Display.cpp
@@ -1583,8 +1583,8 @@ bool asdp::render::DisplayOpenXR::DisplayOpenXRImpl::OpenXRRenderLayer(XrTime pr
     vri.rightHalfFOV = glm::degrees(m_views[i].fov.angleRight);
     vri.topHalfFOV = glm::degrees(m_views[i].fov.angleUp);
     vri.bottomHalfFOV = glm::degrees(m_views[i].fov.angleDown);
-    vri.nearClip = 0.5;         /// @todo See if we get this from somewhere
-    vri.farClip = 1000.0;       /// @todo See if we get this from somewhere
+    vri.nearClip = 2.0;         /// @todo See if we get this from somewhere
+    vri.farClip = 1500.0;       /// @todo See if we get this from somewhere, >> 900 max Composite depth
     vri.frameBuffer = m_swapchainFramebuffer;
     vri.colorBuffer = reinterpret_cast<const XrSwapchainImageOpenGLKHR*>(
       m_swapchainImages[m_swapchains[i].handle][swapchainImageIndex])->image;
