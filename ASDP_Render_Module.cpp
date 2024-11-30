defd2c5531d21a826629cda5df1f6a4b6501e360
Russ Taylor <russ@reliasolve.com>
2024-11-30 16:55:23 -0500

    Render 2 frames behind during replay rather than 1, which further smooths playback.

--- ASDP_Render_Module.cpp
+++ ASDP_Render_Module.cpp
@@ -1253,7 +1253,9 @@ int main(int argc, char** argv)
       }
       uint32_t renderOffsetMicroseconds = 0;
       if (replayStreamID != 0) {
-        renderOffsetMicroseconds = 1000000 / displayInfos[i].fps;
+	// Set up to run 2 frames behind the current time, which empirically was much
+	// smoother than a single frame behind.
+        renderOffsetMicroseconds = 2000000 / displayInfos[i].fps;
       }
       std::shared_ptr<Composite> composite = std::make_shared<CompositeCameras>(
         visibleCameras, toneMapTexture, poseAdjuster, renderOffsetMicroseconds,
