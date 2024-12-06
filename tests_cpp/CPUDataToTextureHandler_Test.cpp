a323d9752db930b15e7a8bf4f76eb0569195c426
Russ Taylor <russ@reliasolve.com>
2024-12-06 09:18:54 -0500

    Using GL_CLAMP_TO_EDGE rather than GL_CLAMP throughout the code.

--- tests_cpp/CPUDataToTextureHandler_Test.cpp
+++ tests_cpp/CPUDataToTextureHandler_Test.cpp
@@ -182,8 +182,8 @@ int main()
     glGenTextures(1, &texture);
     glBindTexture(GL_TEXTURE_2D, texture);
     // Set the texture wrapping parameters
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
     // Set texture filtering parameters
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
