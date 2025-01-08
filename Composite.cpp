a743db64d20a08ee7dcc662a94a2c96518734963
Russ Taylor <russ@reliasolve.com>
2025-01-08 16:28:23 -0500

    Using correct call to bind depth buffer to render buffer

--- Composite.cpp
+++ Composite.cpp
@@ -53,7 +53,7 @@ void Composite::Render(asdp::Time scanOutTime, std::vector<ViewRenderInfo> views
       glBindFramebuffer(GL_FRAMEBUFFER, view.frameBuffer);
       if (view.frameBuffer != 0) {
         glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, view.colorBuffer, 0);
-        glFramebufferTexture2D(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_TEXTURE_2D, view.depthBuffer, 0);
+        glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_RENDERBUFFER, view.depthBuffer);
       }
 
       // Clear the buffers.  The clear color is sky blue to distingiush from a camera with a black texture.
