948b2036ec80d6874e369014d4cc47b22c64bff0
Russ Taylor <russ@reliasolve.com>
2025-02-13 15:07:37 -0500

    Clamping textures to edge in test case to match the rest of the code

--- tests_cpp/CompositeCameras_Test.cpp
+++ tests_cpp/CompositeCameras_Test.cpp
@@ -39,8 +39,8 @@ GLuint MakeTexture(int width, int height, uint16_t minVal, uint16_t maxVal)
   glGenTextures(1, &texture);
   glBindTexture(GL_TEXTURE_2D, texture);
   // Set the texture wrapping parameters
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP);
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
   // Set texture filtering parameters
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
