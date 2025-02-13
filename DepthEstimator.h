293113c6615962020c588f72290d0af948eaae6a
Russ Taylor <russ@reliasolve.com>
2025-02-13 13:42:50 -0500

    Changing default fitness threshold for depth from 5 to 10

--- DepthEstimator.h
+++ DepthEstimator.h
@@ -52,7 +52,7 @@ namespace asdp {
         std::shared_ptr<PoseAdjuster> poseAdjuster, Time cameraFrameInterval,
         unsigned nx, unsigned ny,
         std::vector<float> depths = {2, 5, 10, 20, 50, 100, 200},
-        float fitnessThreshold = 5.0f);
+        float fitnessThreshold = 10.0f);
 
       virtual ~DepthEstimator() = default;
 
