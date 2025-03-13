cd916ff845da36b038bcff4d003c913f5745ed1b
Russ Taylor <russ@reliasolve.com>
2025-03-13 17:53:26 -0400

    Generalizing Display_Test to support testing OpenXR and XSight displays.

--- tests_cpp/Display_Test.cpp
+++ tests_cpp/Display_Test.cpp
@@ -5,14 +5,39 @@
 #include <iostream>
 #include <vector>
 #include <memory>
+#include <string>
 #include <Composite.h>
 #include <Display.h>
 #include <ASDP_Core_API.h>
 
-int main()
+static void usage(const char* progName)
+{
+  std::cerr << "Usage: " << progName << " [--openXR] [--xSight <NIC name>]" << std::endl;
+}
+
+int main(int argc, char** argv)
 {
   int width = 640;
   int height = 640;
+
+  bool useOpenXR = false;
+  std::string xSightNICName = "";
+  for (int i = 1; i < argc; ++i) {
+    if (std::string("--openXR") == argv[i]) {
+      useOpenXR = true;
+    }
+    else if (std::string("--xSight") == argv[i]) {
+      if (++i >= argc) {
+        usage(argv[0]);
+        return 1;
+      }
+      xSightNICName = argv[i];
+    }
+    else {
+      usage(argv[0]);
+      return 2;
+    }
+  }
 
   asdp::render::ViewRenderInfo viewRenderInfo;
   viewRenderInfo.width = width;
@@ -36,37 +61,62 @@ int main()
       return 3;
     }
 
-    // Create a Display window to show the CompositeCube object that shares objects with the texWindow.
-    // Control it using joystick 0.
-    asdp::render::DisplayWindow window("Display_Test", composite, client, 0, 0, 0, 60.0f, 2500, width, height,
-      90, "GLFW::0", &texWindow);
-    if (window.GetStatus() != "") {
-      std::cerr << "Error opening first display: " << window.GetStatus() << std::endl;
-      return 1;
+    std::vector< std::shared_ptr<asdp::render::Display> > displays;
+
+    // Create the appropriate Display object(s) based on the command-line arguments.
+    if (useOpenXR) {
+      displays.push_back(std::make_shared<asdp::render::DisplayOpenXR>(composite, &texWindow, client,
+        0, 0, 0, 2500, 0, nullptr, nullptr, nullptr, false));
+    } else if (xSightNICName != "") {
+      displays.push_back(std::make_shared<asdp::render::DisplayXSight>(xSightNICName, composite, &texWindow, client,
+        0, 0, 0));
+    } else {
+      // Create a Display window to show the CompositeCube object that shares objects with the texWindow.
+      // Control it using joystick 0.
+      displays.push_back(std::make_shared<asdp::render::DisplayWindow>("Display_Test", composite, client,
+        0, 0, 0, 60.0f, 2500, width, height, 90, "GLFW::0", &texWindow));
+      if (displays.back()->GetStatus() != "") {
+        std::cerr << "Error opening first display: " << displays.back()->GetStatus() << std::endl;
+        return 1;
+      }
+
+      // Create a second Display window to show the same CompositeCube object that shares objects
+      // with the texWindow (and therefore the first Display window).
+      // Control it using joystick 1.
+      displays.push_back(std::make_shared<asdp::render::DisplayWindow>("Display_Test2", composite, client,
+        0, 0, 0, 60.0f, 2500, width, height,
+        90, "GLFW::1", &texWindow));
+      if (displays.back()->GetStatus() != "") {
+        std::cerr << "Error opening second display: " << displays.back()->GetStatus() << std::endl;
+        return 2;
+      }
+
+      // Loop until the user closes all displays.
+      std::cout << "You should see a square of varying-brightness green squares in two windows." << std::endl;
+      std::cout << "You should be able to move and resize the windows, with the display updating." << std::endl;
+      std::cout << "You should be able to rotate the views by pressing the arrow keys." << std::endl;
+      std::cout << "Close the windows to exit." << std::endl;
     }
 
-    // Create a second Display window to show the same CompositeCube object that shares objects
-    // with the texWindow (and therefore the first Display window).
-    // Control it using joystick 1.
-    asdp::render::DisplayWindow window2("Display_Test2", composite, client, 0, 0, 0, 60.0f, 2500,
-      width, height,
-      90, "GLFW::1", &texWindow);
-    if (window2.GetStatus() != "") {
-      std::cerr << "Error opening second display: " << window2.GetStatus() << std::endl;
-      return 2;
-    }
-
-    // Done with the composite object -- let the display objects take over destroying it+.
+    // Done with the composite object -- let the display objects take over destroying it.
     composite.reset();
 
-    // Loop until the user closes both windows.
-    std::cout << "You should see a square of varying-brightness green squares in two windows." << std::endl;
-    std::cout << "You should be able to move and resize the windows, with the display updating." << std::endl;
-    std::cout << "You should be able to rotate the views by pressing the arrow keys." << std::endl;
-    std::cout << "Close the windows to exit." << std::endl;
     auto start = std::chrono::steady_clock::now();
-    while ((window.GetStatus() == "") || (window2.GetStatus() == "")) {
+    bool done = false;
+    while (!done) {
       std::this_thread::sleep_for(std::chrono::milliseconds(100));
+
+      // If all of our Displays have been closed (or are broken), then we're done.
+      bool allClosed = true;
+      for (auto& display : displays) {
+        if (display->GetStatus() == "") {
+          allClosed = false;
+          break;
+        }
+      }
+      if (allClosed) {
+        done = true;
+      }
     }
   }
 
