8951383c5a38a7145676547f397d8df09188bdad
Russ Taylor <russ@reliasolve.com>
2026-04-17 13:21:40 -0400

    Bumping Render Module version to 3.28 to indicate the presence of the analysis reconnect and 'a' button

--- ASDP_Render_Module.cpp
+++ ASDP_Render_Module.cpp
@@ -56,7 +56,7 @@ using namespace asdp::render;
 using namespace asdp::analysis;
 using json = nlohmann::json;
 
-static std::string VERSION = "3.27.0";
+static std::string VERSION = "3.28.0";
 
 /// @brief The path to the configuration file. Defined in the CMakeLists file.
 std::filesystem::path g_dirPath = CONFIG_FILE_PATH;
