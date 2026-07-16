e230cdb7d0f5f69537f29ab86b13afb5be33dd3b
Russ Taylor <russ@reliasolve.com>
2026-07-16 14:10:45 -0400

    Fixing file name

--- util/camera_6_cal_scripts/narrow/1_recurse.sh
+++ util/camera_6_cal_scripts/narrow/1_recurse.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-/home/rtaylor/build/ASDP_Render_Module/Target_Calibration_Make_Scan --frames 3 narrow_opt.json targets_lateral_opt.json gimbal.json
+/home/rtaylor/build/ASDP_Render_Module/Target_Calibration_Make_Scan --frames 3 narrow.json targets_lateral_opt.json gimbal.json
