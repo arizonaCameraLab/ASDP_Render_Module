3e6fd15f2e6624d43d09b6d98c0567c336691e2a
Russ Taylor <russ@reliasolve.com>
2026-07-16 13:33:17 -0400

    Removing inversion on brightfield cameras

--- util/camera_6_cal_scripts/wide/4.sh
+++ util/camera_6_cal_scripts/wide/4.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-/home/rtaylor/build/ASDP_Render_Module/Target_Calibration_Estimate_Lateral wide.json targetConfig.json gimbal.json --invert 45000 .
+/home/rtaylor/build/ASDP_Render_Module/Target_Calibration_Estimate_Lateral wide.json targetConfig.json gimbal.json 45000 .
