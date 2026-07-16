98b3ca8759ebba0735e5333b3ecc4318af856906
Russ Taylor <russ@reliasolve.com>
2026-07-16 14:30:22 -0400

    Fixing file names on other scripts

--- util/camera_6_cal_scripts/narrow/4_recurse.sh
+++ util/camera_6_cal_scripts/narrow/4_recurse.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-/home/rtaylor/build/ASDP_Render_Module/Target_Calibration_Estimate_Lateral narrow_opt.json targets_lateral_opt.json gimbal.json --invert 55000 .
+/home/rtaylor/build/ASDP_Render_Module/Target_Calibration_Estimate_Lateral cameras_opt.json targets_lateral_opt.json gimbal.json --invert 55000 .
--- util/camera_6_cal_scripts/wide/4_recurse.sh
+++ util/camera_6_cal_scripts/wide/4_recurse.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-/home/rtaylor/build/ASDP_Render_Module/Target_Calibration_Estimate_Lateral wide_opt.json targets_lateral_opt.json gimbal.json 45000 .
+/home/rtaylor/build/ASDP_Render_Module/Target_Calibration_Estimate_Lateral cameras_opt.json targets_lateral_opt.json gimbal.json 45000 .
